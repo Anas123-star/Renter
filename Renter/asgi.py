c40bf1d53e87c898b518bcfab8dc66cfabea63bb
Anas123-star <khanmohammadanas27@gmail.com>
2023-08-07 06:22:52 +0530

    d

--- Renter/asgi.py
+++ Renter/asgi.py
@@ -5,18 +5,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
+
+
+"""
+from django.conf import settings
+settings.configure()
 import os
 import django
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from home.consumers import ChatConsumer, NotificationsConsumer, TenantsConsumer
-from channels.auth import AuthMiddlewareStack
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Renter.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Renter.settings")
 django.setup()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from home.consumers import ChatConsumer, NotificationsConsumer, TenantsConsumer\
+
+
 application = get_asgi_application()
 # ws_patterns = [
 #     path('chat/<room_code>', ChatConsumer.as_asgi()),
