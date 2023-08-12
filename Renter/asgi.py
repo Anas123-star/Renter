86bb98afafb07c2b4f62f634165ba22a5c2d1426
Anas123-star <khanmohammadanas27@gmail.com>
2023-08-12 19:29:01 +0530

    jk

--- Renter/asgi.py
+++ Renter/asgi.py
@@ -9,32 +9,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 
 """
-from django.conf import settings
-settings.configure()
+
 import os
 import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Renter.settings')
 django.setup()
+
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter,get_default_application
 from django.core.asgi import get_asgi_application
+
 from django.urls import path
-from home.consumers import ChatConsumer, NotificationsConsumer, TenantsConsumer\
+from home.consumers import ChatConsumer, NotificationsConsumer, TenantsConsumer
 
 
 application = get_asgi_application()
-# ws_patterns = [
-#     path('chat/<room_code>', ChatConsumer.as_asgi()),
-#     path('view_message', NotificationsConsumer.as_asgi())
-# ]
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(), 
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter(ws_patterns)
-#         )
-# })
 
 application = ProtocolTypeRouter(
     {
